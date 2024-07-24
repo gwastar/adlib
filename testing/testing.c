d524e14b67f514358643eef8c01b2f361d7e3321
Fabian Hügel <fhuegel97@gmail.com>
2024-07-24 18:34:59 +0200

    testing: Pull out list code into a header

--- testing/testing.c
+++ testing/testing.c
@@ -37,6 +37,7 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include "list.h"
 #include "testing.h"
 
 static uint64_t global_seed;
@@ -158,52 +159,6 @@ void test_log(const char *fmt, ...)
 	vprintf(fmt, args);
 	va_end(args);
 }
-
-struct list_head {
-	struct list_head *prev;
-	struct list_head *next;
-};
-
-static struct list_head list_head_init(struct list_head *list)
-{
-	list->prev = list;
-	list->next = list;
-	return *list;
-}
-
-static bool list_empty(struct list_head *list)
-{
-	return list->next == list;
-}
-
-static void list_push_tail(struct list_head *list, struct list_head *item)
-{
-	item->prev = list->prev;
-	item->next = list;
-	list->prev->next = item;
-	list->prev = item;
-}
-
-static void list_remove_item(struct list_head *item)
-{
-	item->prev->next = item->next;
-	item->next->prev = item->prev;
-	item->prev = (struct list_head *)(uintptr_t)0xdeadbeef;
-	item->next = (struct list_head *)(uintptr_t)0xdeadbeef;
-}
-
-static struct list_head *list_pop_head(struct list_head *list)
-{
-	if (list_empty(list)) {
-		return NULL;
-	}
-	struct list_head *item = list->next;
-	list_remove_item(item);
-	return item;
-}
-
-#define list_foreach(list, itername)					\
-	for (struct list_head *itername = (list)->next; itername != (list); itername = itername->next)
 
 struct worker {
 	struct list_head link;
